bcf0f591f954494969566ebe885fcdb132a42f4e
Justinas Sakalavicius <Justinas Sakalavicius>
2023-07-17 18:08:08 +0300

    Add project files.

---
+++ AWSLambda2/Function.cs
@@ -0,0 +1,74 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using System.Net;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambda2;
+
+public class Function
+{
+    IAmazonS3 S3Client { get; set; }
+
+    /// <summary>
+    /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+    /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+    /// region the Lambda function is executed in.
+    /// </summary>
+    public Function()
+    {
+        S3Client = new AmazonS3Client();
+    }
+
+    /// <summary>
+    /// Constructs an instance with a preconfigured S3 client. This can be used for testing outside of the Lambda environment.
+    /// </summary>
+    /// <param name="s3Client"></param>
+    public Function(IAmazonS3 s3Client)
+    {
+        this.S3Client = s3Client;
+    }
+
+    /// <summary>
+    /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+    /// to respond to S3 notifications.
+    /// </summary>
+    /// <param name="evnt"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public APIGatewayProxyResponse FunctionHandler(S3Event evnt, ILambdaContext context)
+    {
+        var eventRecords = evnt.Records ?? new List<S3Event.S3EventNotificationRecord>();
+        foreach (var record in eventRecords)
+        {
+            var s3Event = record.S3;
+            if (s3Event == null)
+            {
+                continue;
+            }
+
+            try
+            {
+                var key = record.S3.Object.Key;
+                context.Logger.LogLine($"Image Uploaded: {key}");
+            }
+            catch (Exception ex)
+            {
+                context.Logger.LogError($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+                context.Logger.LogError(ex.Message);
+                context.Logger.LogError(ex.StackTrace);
+                throw;
+            }
+        }
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = "",
+            IsBase64Encoded = false
+        };
+    }
+}
